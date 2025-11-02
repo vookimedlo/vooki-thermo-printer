baa3d0d17f03007f9b5f335d40eb5189da0b1d2b
Michal Duda <github@vookimedlo.cz>
2025-11-02 21:24:28 +0100

    🔩Code: D11_H status workaround.

--- src/VookiThermoPrinter/App/AppLogic+D11H.swift
+++ src/VookiThermoPrinter/App/AppLogic+D11H.swift
@@ -41,19 +41,26 @@ extension AppLogic: AppLogicPrinting {
         }
         
         let max = data.count
-        var currentStep = 1
+        var currentStep: UInt16 = 1
         for packet in data {
+            Self.logger.error("Image data packet \(currentStep)/\(max)")
+
             try printer?.setPrinterData(data: packet)
             notifyUI(name: .App.UI.printSendingProgress,
                      userInfo: [String : Sendable](dictionaryLiteral: (Notification.Keys.value, currentStep != max ? Double(currentStep) / Double(max) * 100.0 : 100.0)))
-            currentStep += 1
-            try? await Task.sleep(for: .milliseconds(50),
+            try? await Task.sleep(for: .milliseconds(30),
                                   tolerance: .milliseconds(25))
+
+            currentStep += 1
         }
         
         try await SendAndWaitAsync.waitOnBoolResult(name: .App.endPagePrint) {
             try await self.printer?.endPagePrint()
         }
+        
+        // It seems that the status is correct after 5th status packet with 50ms gap when the second page is sent.
+        // So just wait 400ms.
+        try await Task.sleep(for: .milliseconds(400))
         
         try await SendAndWaitAsync.waitOnBoolResult(name: .App.printFinished,
                                                     timeout: .seconds(10)) {
